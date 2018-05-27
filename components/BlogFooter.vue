8333812f8c2c68c6f0ac98357449485de5ac6608
Ryan <omullan.ryan@gmail.com>
2018-05-27 15:03:49 -0300

    Add footer

---
+++ components/BlogFooter.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <div class="content has-text-centered">
+        <p>
+          Built with <a href="https://bulma.io">Bulma</a> & <a href="https://nuxtjs.org/">Nuxt.js</a><br>
+          Hosted on <a href="https://www.netlify.com/">Netlify</a>
+        </p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+    export default {
+        name: "blogfooter"
+    }
+</script>
+
+<style scoped>
+
+</style>
