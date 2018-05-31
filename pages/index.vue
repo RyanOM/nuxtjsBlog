b0e3aa85b8e9d990bd207248edf08f7b641fcaeb
Ryan <omullan.ryan@gmail.com>
2018-05-31 19:57:59 -0300

    Use postbox component

--- pages/index.vue
+++ pages/index.vue
@@ -7,41 +7,7 @@
           <hr>
 
           <div class="columns is-multiline">
-            <div class="column is-half" v-for="(post, index) in posts" :key="index">
-              <div class="card">
-                <div class="card-image">
-                  <figure class="image is-4by3">
-                    <nuxt-link :to="post.fields.slug">
-                      <img :src="post|getPostImageUrl" alt="Placeholder image" style="object-fit: cover">
-                    </nuxt-link>
-                  </figure>
-                </div>
-                <div class="card-content">
-                  <div class="media">
-                    <div class="media-content">
-                      <p class="title is-4">
-                        <nuxt-link :to="post.fields.slug" class="has-text-dark">
-                          {{ post.fields.title }}
-                        </nuxt-link>
-                      </p>
-                    </div>
-                  </div>
-
-                  <div class="content">
-                    {{ post.fields.content|removeMarkdown|truncate(120) }}
-                    <span v-if="'tags' in post.fields">
-                      <br>
-                      <br>
-                      <i class="fa fa-tags"></i> &nbsp;
-                      <span v-for="(tag, index) in post.fields.tags" :key="index">#{{ tag.fields.name }}</span> <span v-if="index+1 < post.fields.tags.length"> - </span>
-                    </span>
-                    <br>
-                    <br>
-                    <time datetime="">{{ new Date(post.sys.createdAt).toDateString() }}</time>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <postBox v-for="post in posts" :key="post.fields.slug" v-bind:post="post"></postBox>
           </div>
 
         </div>
@@ -52,9 +18,14 @@
 
 <script>
   import client from '~/plugins/contentful';
+  import PostBox from '~/components/PostBox'
 
   export default {
-    asyncData({ params }) {
+    components: {
+      PostBox
+    },
+
+    asyncData({params}) {
       return client
         .getEntries({
           content_type: 'post',
